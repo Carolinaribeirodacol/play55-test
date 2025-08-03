08520824ae1566ce882e01f883d9040de3fb20cf
Carolina Ribeiro <carolinaribeirodacol@gmail.com>
2025-08-03 03:41:52 -0300

    removido arquivo nao utilizadop + banner

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
-    <div class="overflow-hidden shadow-lg shadow-black/10 relative h-64 sm:h-80 md:h-[400px]">
-      <NuxtImg src="public:/images/banner.jpg" alt="Promoção de inverno" class="object-cover w-full h-full" />
+    <div class="overflow-hidden shadow-lg shadow-black/10 relative">
+      <img src="/images/banner.jpg" alt="Banner" class="w-full h-auto" />
     </div>
   </section>
 </template>
